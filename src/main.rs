39c746193dc336886dae7348562d8250db2bd6d3
jvo203 <chris.zapart@nao.ac.jp>
2018-03-16 18:26:35 +0900

    an update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("SubaruWebQL");
+extern crate iron;
+extern crate router;
+extern crate mount;
+extern crate staticfile;
+extern crate urlencoded;
+
+use std::path::Path;
+use staticfile::Static;
+use mount::Mount;
+use router::Router;
+
+use iron::prelude::*;
+use iron::status;
+use urlencoded::UrlEncodedQuery;
+
+fn subaru_handler(req: &mut Request) -> IronResult<Response> {    
+    println!("SubaruWebQL.html request handler");    
+
+    match req.get_ref::<UrlEncodedQuery>() {
+        Ok(ref hashmap) => println!("Parsed GET request query string:\n {:?}", hashmap),
+        Err(ref e) => println!("{:?}", e)
+    };
+    
+    Ok(Response::with((status::Ok, "SubaruWebQL request was routed!")))
+}
+
+//static SERVER_STRING: &'static str ="SubaruWebQL v1.1.0"
+//const VERSION_STRING = "SV2018-03-14.0"
+
+fn main() {    
+    /*let mut iron = Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "SubaruWebQL")))
+    });*/
+
+    let mut router = Router::new();    
+    router.get("/", subaru_handler, "votable");
+    
+    let mut mount = Mount::new();    
+    mount.mount("/subaruwebql/SubaruWebQL.html", router);    
+    mount.mount("/", Static::new(Path::new("target/htdocs/subaru.html")));
+    mount.mount("/favicon.ico", Static::new(Path::new("target/htdocs/favicon.ico")));
+    mount.mount("/subaruwebql/", Static::new(Path::new("target/htdocs/subaruwebql/")));
+    
+    let mut iron = Iron::new(mount);
+    iron.threads = 4;
+
+    println!("SubaruWebQL daemon started.");
+    iron.http("localhost:8081").unwrap();
+    println!("SubaruWebQL daemon ended.");//need to find a way to intercept CTRL+C as the last line does not get printed
 }
---
+++ target/debug/deps/subaru_web_ql-ff818ceaa0541fbe.d
@@ -0,0 +1,5 @@
+/home/chris/projects/subaru_web_ql/target/debug/deps/subaru_web_ql-ff818ceaa0541fbe: src/main.rs
+
+/home/chris/projects/subaru_web_ql/target/debug/deps/subaru_web_ql-ff818ceaa0541fbe.d: src/main.rs
+
+src/main.rs:
